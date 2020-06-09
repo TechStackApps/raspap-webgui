ce1ef90f8383fef6d9a2198402b14d5151cfc1d1
fjhunter <falkjaeger99@gmail.com>
2020-06-09 17:47:25 +0100

    #602: Adds rest endpoints for adblock config and wifistations

--- ajax/networking/wifi_stations.php
+++ ajax/networking/wifi_stations.php
@@ -1,18 +1,7 @@
 <?php
 
-require '../../includes/csrf.php';
-require_once '../../includes/config.php';
-require_once '../../includes/defaults.php';
-require_once '../../includes/functions.php';
 require_once '../../includes/wifi_functions.php';
 
-$networks = [];
-$network  = null;
-$ssid     = null;
-
-knownWifiStations($networks);
-nearbyWifiStations($networks, !isset($_REQUEST["refresh"]));
-connectedWifiStations($networks);
-sortNetworksByRSSI($networks);
+$networks = getWifiStations();
 
 echo renderTemplate('wifi_stations', compact('networks'));
